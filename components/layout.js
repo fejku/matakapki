b6783515959b2f9e85a9bc7f98fd8d11cfe1c962
fejku <przecinek45@o2.pl>
2021-04-14 00:37:38 +0200

    Dodany layout o indexy

---
+++ components/layout.js
@@ -0,0 +1,39 @@
+import styles from "../styles/Layout.module.css";
+import Link from "next/link";
+import Head from "next/head";
+
+export default function Layout({ children, title = "This is the default title" }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a className={styles.logoLink}>
+              <img src="./logo.png" className={styles.logoImg} />
+            </a>
+          </Link>
+        </div>
+
+        <nav>
+          <Link href="/cv">
+            <a>CV</a>
+          </Link>
+          <Link href="/gry">
+            <a>Gry</a>
+          </Link>
+          <Link href="/aplikacje">
+            <a>Aplikacje</a>
+          </Link>
+        </nav>
+      </header>
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
