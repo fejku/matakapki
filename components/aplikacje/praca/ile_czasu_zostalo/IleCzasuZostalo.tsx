a9f185fc68f0cb6d500de41e8951431889341082
Krystian Kalita <krystian.kalita@zetosoftware.pl>
2021-04-16 09:22:18 +0200

    Dodano jako takie stylowanie, przeniesiono całą logikę do klasu Utils

--- components/aplikacje/praca/ile_czasu_zostalo/IleCzasuZostalo.tsx
+++ components/aplikacje/praca/ile_czasu_zostalo/IleCzasuZostalo.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
-import { formatujMinutyNaCzas, podajIloscMinutDlaPrzerwy, zamienCzasNaMinuty } from "./IleCzasuZostaloUtils";
+import styles from "./IleCzasuZostalo.module.css";
+
+import { useEffect, useState } from "react";
+import { wyznaczCzas } from "./IleCzasuZostaloUtils";
+import clsx from "clsx";
 
 interface Props {}
 
@@ -9,6 +12,11 @@ const IleCzasuZostalo: React.FC<Props> = () => {
   const [czyPrzerwa, setCzyPrzerwa] = useState(false);
   const [przerwaPoczatek, setPrzerwaPoczatek] = useState("");
   const [przerwaKoniec, setPrzerwaKoniec] = useState("");
+  const [wynik, setWynik] = useState("");
+
+  useEffect(() => {
+    setWynik(wyznaczCzas(poczatek, czasPracy, czyPrzerwa, przerwaPoczatek, przerwaKoniec));
+  }, [czasPracy, poczatek, czyPrzerwa, przerwaPoczatek, przerwaKoniec]);
 
   const handleCzasPracyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCzasPracy(e.target.value);
@@ -30,38 +38,36 @@ const IleCzasuZostalo: React.FC<Props> = () => {
     setPrzerwaKoniec(e.target.value);
   };
 
-  const wylicz = () => {
-    if (!poczatek) {
-      return "";
-    }
-
-    let wynikWMinutach =
-      zamienCzasNaMinuty(czasPracy) +
-      zamienCzasNaMinuty(poczatek) +
-      podajIloscMinutDlaPrzerwy(czyPrzerwa, przerwaPoczatek, przerwaKoniec);
-
-    return formatujMinutyNaCzas(wynikWMinutach);
-  };
-
   return (
-    <div>
-      <div>
-        <label htmlFor="inputCzasPracy">CzasPracy</label>
+    <div className={styles.IleCzasuZostalo}>
+      <div className={styles.Wiersz}>
+        <label htmlFor="inputCzasPracy">Czas pracy</label>
         <input type="time" name="inputCzasPracy" value={czasPracy} onChange={handleCzasPracyChange} />
       </div>
-      <div>
+      <div className={styles.Wiersz}>
         <label htmlFor="inputPoczatek">Początek</label>
         <input type="time" name="inputPoczatek" value={poczatek} onChange={handlePoczatekChange} />
       </div>
-      <div>
-        <input type="checkbox" checked={czyPrzerwa} onChange={handleCzyPrzerwaChange} />
-        <label htmlFor="inputPrzerwaPoczatek">Przerwa od</label>
-        <input type="time" name="inputPrzerwaPoczatek" value={przerwaPoczatek} onChange={handlePrzerwaPoczatekChange} />
-
-        <label htmlFor="inputPrzerwaKoniec">do</label>
-        <input type="time" name="inputPrzerwaKoniec" value={przerwaKoniec} onChange={handlePrzerwaKoniecChange} />
+      <div className={clsx(styles.Wiersz, styles.Przerwa)}>
+        <div>
+          Przerwa
+          <input type="checkbox" checked={czyPrzerwa} onChange={handleCzyPrzerwaChange} />
+        </div>
+        <div>
+          <label htmlFor="inputPrzerwaPoczatek">od</label>
+          <input
+            type="time"
+            name="inputPrzerwaPoczatek"
+            value={przerwaPoczatek}
+            onChange={handlePrzerwaPoczatekChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="inputPrzerwaKoniec">do</label>
+          <input type="time" name="inputPrzerwaKoniec" value={przerwaKoniec} onChange={handlePrzerwaKoniecChange} />
+        </div>
       </div>
-      <div>Koniec {wylicz()}</div>
+      <div className={styles.Wiersz}>{wynik}</div>
     </div>
   );
 };
--- components/aplikacje/praca/ile_czasu_zostalo/IleCzasuZostaloUtils.ts
+++ components/aplikacje/praca/ile_czasu_zostalo/IleCzasuZostaloUtils.ts
@@ -1,4 +1,4 @@
-export const zamienCzasNaMinuty = (czas: string) => {
+const zamienCzasNaMinuty = (czas: string) => {
   if (czas) {
     const [czasGodzina, czasMinuty] = czas.split(":");
     return parseInt(czasGodzina) * 60 + parseInt(czasMinuty);
@@ -6,7 +6,7 @@ export const zamienCzasNaMinuty = (czas: string) => {
   return 0;
 };
 
-export const podajIloscMinutDlaPrzerwy = (czyPrzerwa: boolean, przerwaPoczatek: string, przerwaKoniec: string) => {
+const podajIloscMinutDlaPrzerwy = (czyPrzerwa: boolean, przerwaPoczatek: string, przerwaKoniec: string) => {
   if (czyPrzerwa && przerwaPoczatek && przerwaKoniec) {
     const iloscMinutPoczatekPrzerwy = zamienCzasNaMinuty(przerwaPoczatek);
     const iloscMinutKoniecPrzerwy = zamienCzasNaMinuty(przerwaKoniec);
@@ -16,11 +16,30 @@ export const podajIloscMinutDlaPrzerwy = (czyPrzerwa: boolean, przerwaPoczatek: 
   return 0;
 };
 
-export const formatujMinutyNaCzas = (minuty: number) => {
+const formatujMinutyNaCzas = (minuty: number) => {
   const wynikGodzina = Math.floor(minuty / 60);
   const wynikGodzinaSformatowana = wynikGodzina < 10 ? `0${wynikGodzina}` : wynikGodzina.toString();
 
   const wynikMinuty = minuty % 60;
 
   return `${wynikGodzinaSformatowana}:${wynikMinuty}`;
+};
+
+export const wyznaczCzas = (
+  poczatek: string,
+  czasPracy: string,
+  czyPrzerwa: boolean,
+  przerwaPoczatek: string,
+  przerwaKoniec: string
+) => {
+  if (poczatek) {
+    const wynikWMinutach =
+      zamienCzasNaMinuty(czasPracy) +
+      zamienCzasNaMinuty(poczatek) +
+      podajIloscMinutDlaPrzerwy(czyPrzerwa, przerwaPoczatek, przerwaKoniec);
+
+    const sformatowanyCzas = formatujMinutyNaCzas(wynikWMinutach);
+    return `Koniec ${sformatowanyCzas}`;
+  }
+  return "Nie podonano godziny rozpoczęcia";
 };
